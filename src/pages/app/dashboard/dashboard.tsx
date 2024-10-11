f9a0afeb9ac82eb6182f7c876238ba86360823c4
Jackson <jacksyntax20@gmail.com>
2024-10-11 19:15:53 -0300

    editing dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,16 +1,5 @@
-import { FormNewTransfer } from '@/components/form-new-transfer'
-import { getUsers } from '@/lib/transfers'
-import { Helmet } from 'react-helmet-async'
+import { Transfers } from './transfers/transfers'
 
 export function Dashboard() {
-  getUsers()
-  return (
-    <>
-      <Helmet title="Nova transferência" />
-      <div className="">
-        <h1 className="text-2xl">Adicionar uma nova transferencia</h1>
-        <FormNewTransfer />
-      </div>
-    </>
-  )
+  return <Transfers />
 }
